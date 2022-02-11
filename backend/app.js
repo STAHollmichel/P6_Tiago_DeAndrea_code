e0ecf48b4d8e2967d296e8d166072f378eda63d0
Tiago de Andrea <sta.holllmichel@gmail.com>
2022-02-11 00:28:01 +0000

    Backend: CORS; doenv &  mongoDB setup

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+require('dotenv').config();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+mongoose.connect(process.env.MONGO_CONNECT,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.post('/api/sauce', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
